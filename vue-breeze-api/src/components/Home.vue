c98926b0b1a5d02f8f8d344dd57b76a73bf1710b
SHENEL98 <wsakurera@gmail.com>
2024-03-11 19:10:56 +0100

    Login and display user details

--- vue-breeze-api/src/components/Home.vue
+++ vue-breeze-api/src/components/Home.vue
@@ -1,5 +1,18 @@
+<script setup>
+ import { ref, onMounted} from 'vue';
+ import axios from 'axios';
+
+ const user = ref();
+ onMounted(async() => {
+    const data = await axios.get('/api/user');
+    user.value = data.data;
+    console.log(user.value);
+ });
+
+</script>
 <template>
     <div>
-        
+        <h1>{{ user?.name }}</h1>
+        <h1>{{ user?.email }}</h1>
     </div>
 </template>
--- vue-breeze-api/src/components/Login.vue
+++ vue-breeze-api/src/components/Login.vue
@@ -1,4 +1,21 @@
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { routeLocationKey, useRouter} from 'vue-router'
+
+const form = ref({
+  email: '',
+  password: ''
+});
+
+const handleLogin = async() =>{
+  await axios.post('/login', {
+    email: form.value.email,
+    password: form.value.password
+  });
+  routerKey.push("/");
+};
+
 
 </script>
 <template> 
@@ -9,11 +26,13 @@
       </div>
   
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
+        <form class="space-y-6" @submit.prevent="handleLogin">
           <div>
             <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
             <div class="mt-2">
-              <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input id="email" name="email" type="email" 
+                v-model="form.email"
+                autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
   
@@ -25,7 +44,7 @@
               </div>
             </div>
             <div class="mt-2">
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input id="password" name="password" type="password" v-model="form.password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
   
