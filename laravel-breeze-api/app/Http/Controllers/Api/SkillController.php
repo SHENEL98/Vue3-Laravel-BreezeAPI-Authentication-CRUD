594a0b32767603d270de71c7cd9dc081cd999cb8
Shenel Kurera <wsakurera@gmail.com>
2024-04-12 21:19:21 +0200

    Store method

--- laravel-breeze-api/app/Http/Controllers/Api/SkillController.php
+++ laravel-breeze-api/app/Http/Controllers/Api/SkillController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSkillRequest;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
     public function index(){
         return response()->json("Skill Index");
+    }
+    public function store(StoreSkillRequest $request){
+        Skill::create($request->validated());
+        return response()->json("Skill Created");
     }
 }
