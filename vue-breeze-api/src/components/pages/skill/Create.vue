7408bcaa87419effc21dd301a729ec817f5873d9
Shenel Kurera <wsakurera@gmail.com>
2024-04-18 23:21:36 +0200

    apply error message

--- vue-breeze-api/src/components/pages/skill/Create.vue
+++ vue-breeze-api/src/components/pages/skill/Create.vue
@@ -8,12 +8,18 @@
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray">Name</label>
                     <input type="text" id="name" v-model="form.name"
                         class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <div v-if="errors.name">
+                        <span class="text-sm text-red-400">{{ errors.name[0] }}</span>
+                    </div>
                 </div>
                 <div class="mb-6">
                     <label for="slug"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray">Slug</label>
                     <input type="text" id="slug" v-model="form.slug"
                         class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <div v-if="errors.slug">
+                        <span class="text-sm text-red-400">{{ errors.slug[0] }}</span>
+                    </div>
                 </div>
                 <div class="mt-4">
                     <button type="submit" class="bg-blue">Save</button>
